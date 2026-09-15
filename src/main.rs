ef0b604898977ae1357aafaa7bbabc69bcc0013e
Tomasz Orda <tomaszorda@outlook.com>
2026-09-15 22:18:56 +0200

    Generate an ESP32C3 template using esp-rs/esp-generate

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm::delay;
-use cortex_m_rt::entry;
-use panic_halt as _;
-use stm32l0xx_hal::{pac, prelude::*, rcc::Config};
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use esp_hal::gpio::{Output, OutputConfig};
+use core::panic::PanicInfo;
 
-#[entry]
-fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let mut led = Output::new(peripherals.GPIO0,esp_hal::gpio::Level::Low, OutputConfig::default());
+    loop{
+        Timer::after(Duration::from_secs(2)).await;
+        led.set_high();
+        Timer::after(Duration::from_secs(2)).await;
+        led.set_low();
 
-    let mut rcc = dp.RCC.freeze(Config::hsi16());
-
-    let gpiob = dp.GPIOB.split(&mut rcc);
-    let mut led = gpiob.pb3.into_push_pull_output();
-
-    loop {
-        led.set_low().unwrap();
-        delay(1_000_000);
-        led.set_high().unwrap();
-        delay(1_000_000);
-        
     }
+    // let dp = pac::Peripherals::take().unwrap();
+
+    // let mut rcc = dp.RCC.freeze(Config::hsi16());
+
+    // let gpiob = dp.GPIOB.split(&mut rcc);
+    // let mut led = gpiob.pb3.into_push_pull_output();
+
+    // loop {
+    //     led.set_low().unwrap();
+    //     delay(1_000_000);
+    //     led.set_high().unwrap();
+    //     delay(1_000_000);
+        
+    // }
+}
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // let mut host_stderr = HStderr::new();
+
+    // logs "panicked at '$reason', src/main.rs:27:4" to the host stderr
+    // writeln!("something", "{}", info).ok();
+
+    loop {}
 }
